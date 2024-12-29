4d6e04e1782cb3033c066d94f7afe38e28661ca3
vaibhavfunde <139740481+vaibhavfunde@users.noreply.github.com>
2024-12-29 14:42:03 +0530

    page.js

--- app/page.js
+++ app/page.js
@@ -39,7 +39,7 @@ export default function Home() {
 
           {/* Watch Video Button */}
           <a
-            href="https://youtu.be/Q5LM985yUmQ"
+            href="/#"
             className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
           >
             <svg
